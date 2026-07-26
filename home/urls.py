77709a53868b1cdd0dc714b84abbc9c20a77dc24
AIIOB-PC <aiiob@icloud.com>
2026-07-26 04:10:20 +0300

    99.9% download

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 app_name = 'home'
 
 urlpatterns = [
   path('', views.IndexView.as_view(), name='index'),
+  path('about/', TemplateView.as_view(template_name='home/about.html'), name='about'),
+  path('contact/', TemplateView.as_view(template_name='home/contact.html'), name='contact'),
 ]
