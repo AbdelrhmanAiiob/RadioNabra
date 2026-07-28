a7fddfa73b341b9130d5d3777937c6353f5a4e76
AIIOB-PC <aiiob@icloud.com>
2026-07-28 06:54:25 +0300

    googel seo verv

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from home.views import coming_soon
+from django.views.generic import TemplateView
 
 urlpatterns = [
+  path('googled0a8a8e73a7d08e9.html', TemplateView.as_view(template_name='googled0a8a8e73a7d08e9.html')),
+  
     path('nabra-control-admin', admin.site.urls),
     path('coming_soon', coming_soon, name='coming_soon'),
     path('', include('home.urls'), name='home'),
